96815790a9ce60f9ec5234acc20deeac821f853e
Chun <132124373+Chungus1310@users.noreply.github.com>
2025-02-12 07:25:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from amazon_scraper import scrape_amazon_products
 from yandex_scraper import scrape_yandex_market_items
+import os
 
 app = Flask(__name__)
 
@@ -35,4 +36,5 @@ def search():
         }), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
